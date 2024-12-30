0cc8af4bc7b60584e49303a6a37036f4a11ae014
Shadow <nicolafabian75@gmail.com>
2024-12-30 10:36:09 +0100

    Add handling for new player account creation on login
    Introduced a PlayerLoginEvent listener to check if a player's account exists in the database. If not, a new BankUser account is created and initialized with default values. This ensures all players have an account upon their first login.

--- src/main/java/de/varilx/vaxbank/listener/ConnectionListener.java
+++ src/main/java/de/varilx/vaxbank/listener/ConnectionListener.java
@@ -1,17 +1,46 @@
 package de.varilx.vaxbank.listener;
 
 
+import de.varilx.database.repository.Repository;
 import de.varilx.vaxbank.VBank;
+import de.varilx.vaxbank.user.BankUser;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ConnectionListener implements Listener {
 
     VBank plugin;
+    Repository<BankUser, UUID> userRepository;
 
     public ConnectionListener(VBank plugin) {
         this.plugin = plugin;
+        this.userRepository = (Repository<BankUser, UUID>) plugin.getDatabaseService().getRepositoryMap().get(BankUser.class);
     }
+
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+        userRepository.exists(player.getUniqueId()).thenAccept(status -> {
+           if(status) return;
+           BankUser bankUser = new BankUser();
+           bankUser.setBalance(0.0);
+           bankUser.setName(player.getName());
+           bankUser.setUniqueId(player.getUniqueId());
+           bankUser.setTransactions(new ArrayList<>());
+           userRepository.insert(bankUser);
+
+        }).exceptionally(throwable -> {
+            throwable.printStackTrace();
+            return null;
+        });
+    }
+
 }
