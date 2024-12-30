8e10b15fdaa6eb2a3632bc31ac0bde3d80ce3e36
Shadow <nicolafabian75@gmail.com>
2024-12-30 13:42:30 +0100

    Add validation for positive amounts in bank commands
    Ensure deposit and withdrawal amounts are positive and numeric. Improve user feedback messages for invalid inputs and insufficient funds. Streamline code by refactoring balance updates and repository interactions.

--- src/main/java/de/varilx/vaxbank/commands/BankCommand.java
+++ src/main/java/de/varilx/vaxbank/commands/BankCommand.java
@@ -55,36 +55,76 @@ public class BankCommand extends VaxCommand {
                         return false;
                     }
 
+                    if(amount <= 0) {
+                        player.sendMessage(LanguageUtils.getMessage("input_number_positive"));
+                        return false;
+                    }
+
                     plugin.getEconomy().withdrawPlayer(player, amount);
 
                     userRepository.findFirstById(player.getUniqueId()).thenAccept(user -> {
-                        if(user == null) {
-                            System.out.println("User is null");
-                            return;
-                        }
-                        user.setBalance(user.getBalance() + amount);
+                        user.addBalance(amount);
                         player.sendMessage(LanguageUtils.getMessage("bank_deposit_success",
                                 Placeholder.parsed("amount", NumberUtils.formatDouble(amount)),
                                 Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
                         ));
 
                         BankTransaction transaction = new BankTransaction();
+                        transaction.setAccountId(player.getUniqueId());
                         transaction.setTime(System.currentTimeMillis());
                         transaction.setType(BankTransactionType.ADD);
                         transaction.setAmount(amount);
                         transaction.setBalance(user.getBalance());
                         transaction.setUser(user);
 
-                        //user.getTransactions().add(transaction);
+                        user.addTransaction(transaction);
 
-                        userRepository.insert(user);
+                        userRepository.save(user);
                     }).exceptionally(throwable -> {
                         throwable.printStackTrace();
                         return null;
                     });
 
                 } else if(args[0].toLowerCase().equalsIgnoreCase(LanguageUtils.getMessageString("Commands.BankCommand.Arguments.Remove"))) {
+                    if(!NumberUtils.isNumeric(args[1])) {
+                        player.sendMessage(LanguageUtils.getMessage("input_no_number"));
+                        return false;
+                    }
 
+                    double amount = Double.parseDouble(args[1]);
+
+                    if(amount <= 0) {
+                        player.sendMessage(LanguageUtils.getMessage("input_number_positive"));
+                        return false;
+                    }
+
+
+                    userRepository.findFirstById(player.getUniqueId()).thenAccept(user -> {
+                        if(user.getBalance() < amount) {
+                            player.sendMessage(LanguageUtils.getMessage("not_enough_money"));
+                            return;
+                        }
+
+                        user.removeBalance(amount);
+                        plugin.getEconomy().withdrawPlayer(player, amount);
+                        player.sendMessage(LanguageUtils.getMessage("bank_withdraw_success",
+                                Placeholder.parsed("amount", NumberUtils.formatDouble(amount)),
+                                Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+                        ));
+
+                        BankTransaction transaction = new BankTransaction();
+                        transaction.setAccountId(player.getUniqueId());
+                        transaction.setTime(System.currentTimeMillis());
+                        transaction.setType(BankTransactionType.REMOVE);
+                        transaction.setAmount(amount);
+                        transaction.setBalance(user.getBalance());
+                        transaction.setUser(user);
+                        user.addTransaction(transaction);
+                        userRepository.save(user);
+                    }).exceptionally(throwable -> {
+                        throwable.printStackTrace();
+                        return null;
+                    });
                 }
             }
 
