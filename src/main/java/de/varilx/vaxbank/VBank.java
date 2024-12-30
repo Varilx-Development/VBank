ef3c49f4b8eab0899027a3ca08325ec9f58c6b0f
Shadow <nicolafabian75@gmail.com>
2024-12-30 13:20:14 +0100

    Add economy integration and command/listener registration
    Integrated Vault-based economy setup to ensure compatibility with an economy plugin. Added new commands and listeners, including `BankCommand` and `ConnectionListener`, to enhance plugin functionality. Gracefully disables the plugin if Vault is not found.

--- src/main/java/de/varilx/vaxbank/VBank.java
+++ src/main/java/de/varilx/vaxbank/VBank.java
@@ -1,11 +1,17 @@
 package de.varilx.vaxbank;
 
 import de.varilx.BaseAPI;
+import de.varilx.command.registry.VaxCommandRegistry;
 import de.varilx.database.Service;
+import de.varilx.vaxbank.commands.BankCommand;
+import de.varilx.vaxbank.listener.ConnectionListener;
 import de.varilx.vaxbank.user.BankUser;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.UUID;
@@ -15,20 +21,51 @@ import java.util.UUID;
 public final class VBank extends JavaPlugin {
 
     Service databaseService;
+    Economy economy;
 
     @Override
     public void onEnable() {
         new BaseAPI(this).enable();
         initializeDatabaseService();
+        if (!setupEconomy()) {
+            getLogger().severe("Vault could not be found! Please install Vault and an Economy plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        registerListeners();
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
     }
 
+    private void registerCommands() {
+        VaxCommandRegistry commandRegistry = new VaxCommandRegistry();
+        commandRegistry.registerCommand(new BankCommand(this));
+    }
+
+    private void registerListeners() {
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new ConnectionListener(this), this);
+    }
+
     private void initializeDatabaseService() {
         databaseService = Service.load(BaseAPI.getBaseAPI().getDatabaseConfiguration().getConfig(), getClassLoader());
         databaseService.create(BankUser.class, UUID.class);
+    }
+
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return (economy != null);
     }
 
 }
