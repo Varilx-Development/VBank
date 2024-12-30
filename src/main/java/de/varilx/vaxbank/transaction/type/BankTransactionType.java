5d19e274149c1870e8042c7b53c699ea8584e261
Shadow <nicolafabian75@gmail.com>
2024-12-30 22:42:48 +0100

    Add new admin transaction types with localized display names
    New transaction types (ADMIN_ADD, ADMIN_REMOVE, ADMIN_SET, ADMIN_RESET) were introduced with localized display names provided by LanguageUtils. This enhances functionality and improves display flexibility for administrative operations.

--- src/main/java/de/varilx/vaxbank/transaction/type/BankTransactionType.java
+++ src/main/java/de/varilx/vaxbank/transaction/type/BankTransactionType.java
@@ -1,9 +1,24 @@
 package de.varilx.vaxbank.transaction.type;
 
 
+import de.varilx.utils.language.LanguageUtils;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum BankTransactionType {
 
-    ADD,
-    REMOVE;
+    ADD(LanguageUtils.getMessageString("add")),
+    REMOVE(LanguageUtils.getMessageString("remove")),
+    ADMIN_ADD(LanguageUtils.getMessageString("admin_add")),
+    ADMIN_REMOVE(LanguageUtils.getMessageString("admin_remove")),
+    ADMIN_SET(LanguageUtils.getMessageString("admin_set")),
+    ADMIN_RESET(LanguageUtils.getMessageString("admin_reset"));
 
+    String displayName;
 }
