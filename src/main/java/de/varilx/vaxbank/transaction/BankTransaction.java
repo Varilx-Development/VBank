64b2b39a6bf5e75b98231af990aed2f37013033a
Shadow <nicolafabian75@gmail.com>
2024-12-30 10:36:03 +0100

    Add JPA annotations to BankTransaction class
    This enhancement integrates JPA annotations, enabling the class to function as a database entity. It includes mappings for the transactionId field with a UUID generation strategy. These changes prepare the class for persistence in a database context.

--- src/main/java/de/varilx/vaxbank/transaction/BankTransaction.java
+++ src/main/java/de/varilx/vaxbank/transaction/BankTransaction.java
@@ -1,14 +1,28 @@
 package de.varilx.vaxbank.transaction;
 
-
+import de.varilx.database.id.Id;
 import de.varilx.vaxbank.transaction.type.BankTransactionType;
-import lombok.AccessLevel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.util.UUID;
 
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BankTransaction {
+
+    @Id
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID transactionId;
 
     UUID sender, receiver;
     double amount;
